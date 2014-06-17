5d1f8113ab867569297bf958e88383be83055dd3
Gwen Brinsmead <gwen@Gwens-MacBook-Pro.local>
2014-06-17 01:27:42 -0700

    Initial Commit

---
+++ facebooklogin/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  facebooklogin
+//
+//  Created by Gwen Brinsmead on 6/17/14.
+//  Copyright (c) 2014 Gwen Brinsmead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ facebooklogin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  facebooklogin
+//
+//  Created by Gwen Brinsmead on 6/17/14.
+//  Copyright (c) 2014 Gwen Brinsmead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
